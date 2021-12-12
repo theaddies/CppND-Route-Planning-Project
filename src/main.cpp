d18727ec292121357f8c16031edeff75396718f2
bigdaddy <theaddies@neo.rr.com>
2021-12-12 18:32:35 -0500

    another test change

--- src/main.cpp
+++ src/main.cpp
@@ -66,13 +66,13 @@ int main(int argc, const char **argv)
     std::cin >> end_x;
     std::cout << "enter the y end point\n";
     std::cin >> end_x;
-    
+
 
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
